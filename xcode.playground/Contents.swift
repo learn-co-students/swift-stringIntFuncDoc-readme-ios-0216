e7cf1fb8abdb7852b8c10f1fef6c7649dc15001d
susanlovaglio <susan@flatironschool.com>
2016-07-10 22:36:12 -0400

    I'm up

---
+++ xcode.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "audrey"
+
+let shout =  name.uppercaseString
+
+print(shout)
+
+print("\(name)...\(name)...\(shout)! WAKE UP!")
+
+
+
+
